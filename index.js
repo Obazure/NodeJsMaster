d6e6f90801d0b72fe008935344403d7e8fd689d8
Yeldar Kossynbay <yeldar.kossynbay@tmm-group.de>
2020-08-30 10:49:40 +0200

    Node basics, Path module

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 
-(function (require, module, exports, __filename, __direname){
+// (function (require, module, exports, __filename, __direname){
     const obj = require('./user')
 
     console.log(obj.user)
 
     obj.sayHello()
-})()
+// })()
---
+++ path_ref.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+console.log(__filename)
+console.log(path.basename(__filename))
+console.log(path.dirname(__filename))
+console.log(path.extname(__filename))
+
+// Getting file info
+console.log(path.parse(__filename))
+
+// Folder/File path generation
+console.log(path.join(__dirname, 'test', 'second.html'))
+console.log(path.join(__dirname, './test', '/second.html'))
+
+
+
