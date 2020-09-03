38cbf54df1cfd97129f9bad6f5c911d52267769f
Yeldar Kossynbay <yeldar.kossynbay@tmm-group.de>
2020-09-03 07:05:05 +0200

    make order, clear cart

--- routes/orders.js
+++ routes/orders.js
@@ -4,72 +4,50 @@ const router = Router()
 
 // index
 router.get('/', async (req, res) => {
-    const orders = await Order.find()
-        .populate('userId', 'name email')
-        // .select('')
+    const orders = await Order.find({'user.userId': req.user._id})
+        .populate('user.userId')
         .lean()
     res.render('orders', {
         title: ' Orders',
         isOrders: true,
-        orders
+        orders: orders.map(o => {
+            return {
+                ...o._doc,
+                price: o.courses.reduce((total, c) => {
+                    return total += c.price * c.count
+                }, 0)
+            }
+        })
+
     })
 })
 
 // create
 // store
 router.post('/', async (req, res) => {
-    const course = new Order({
-        title: req.body.title,
-        price: req.body.price,
-        img: req.body.img,
-        userId: req.user
-    })
     try {
-        await course.save()
+        const user = await req.user
+            .populate('cart.items.courseId')
+            .execPopulate()
+        const courses = user.cart.items.map(i => ({
+            count: i.count,
+            course: {...i.courseId._doc}
+        }))
+
+        const order = new Order({
+            user: {
+                name: user.name,
+                userId: user
+            },
+            courses
+        })
+        await order.save()
+        await user.clearCart()
     } catch (e) {
         console.log(e)
     }
+
     res.redirect('/orders')
-})
-
-// show
-router.get('/:id', async (req, res) => {
-    const course = await Course.findById(req.params.id).lean()
-    res.render('course', {
-        layout: 'empty',
-        title: `Course: ${course.title}`,
-        course
-    })
-})
-
-// edit
-router.get('/:id/edit', async (req, res) => {
-    if (!req.query.allow) {
-        return res.redirect('/courses')
-    }
-    const course = await Course.findById(req.params.id).lean()
-    res.render('course-edit', {
-        title: `Edit course: ${course.title}`,
-        course
-    })
-})
-
-// update
-router.post('/:id', async (req, res) => {
-    if (!req.query.allow || !req.params.id) {
-        return res.redirect('/courses')
-    }
-    await Course.findByIdAndUpdate(req.params.id, req.body)
-    res.redirect('/courses')
-})
-
-//destroy
-router.post('/:id/delete', async (req, res) => {
-    if (!req.query.allow || !req.params.id) {
-        return res.redirect('/courses')
-    }
-    await Course.findByIdAndRemove(req.params.id)
-    res.redirect('/courses')
 })
 
 module.exports = router
