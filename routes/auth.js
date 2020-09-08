b7e9946491cef383893ac29491527c313c607020
Obazure <conprefor@gmail.com>
2020-09-08 23:46:38 +0200

    express-validator validator for login

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 const {Router} = require('express')
 const bcrypt = require('bcryptjs')
 const crypto = require('crypto')
-const {validationResult} = require('express-validator/check')
+const {validationResult} = require('express-validator')
 const User = require('../models/user')
 const router = Router()
-const {registerValidators} = require('../utils/validators')
+const {loginValidators, registerValidators} = require('../utils/validators')
 const config = require('../config/config')
 const sgMail = require('@sendgrid/mail');
 const registrationSuccessMessage = require('../views/emails/registration-success')
@@ -20,28 +20,22 @@ router.get('/login', async (req, res) => {
     })
 })
 
-router.post('/login', async (req, res) => {
+router.post('/login', loginValidators, async (req, res) => {
     try {
         const {email, password} = req.body
-        const candidate = await User.findOne({email})
-        if (candidate) {
-            const areSame = await bcrypt.compare(password, candidate.password)
-            if (areSame) {
-                req.session.user = candidate
-                req.session.isAuthenticated = true
-                req.session.save(err => {
-                    if (err) throw err
-                    else res.redirect('/courses')
-                })
-            } else {
-                req.flash('login-error', 'Password is wrong.')
-                res.redirect('/auth/login#login')
-            }
-        } else {
-            req.flash('login-error', 'Email is wrong.')
-            res.redirect('/auth/login#login')
+
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            req.flash('login-error', errors.array()[0].msg)
+            return res.status(422).redirect('/auth/login#login')
         }
 
+        req.session.user = await User.findOne({email: req.body.email})
+        req.session.isAuthenticated = true
+        req.session.save(err => {
+            if (err) throw err
+            else res.redirect('/courses')
+        })
 
     } catch (e) {
         console.log(e)
--- utils/validators.js
+++ utils/validators.js
@@ -2,7 +2,7 @@ const {body, validationResult} = require('express-validator/check')
 const User = require('../models/user')
 
 exports.registerValidators = [
-    body('email').isEmail().withMessage('Wrong Email.')
+    body('email').isEmail().withMessage('Email is wrong.')
         .custom(async (value, {req}) => {
             try {
                 const user = await User.findOne({email: value})
@@ -25,4 +25,33 @@ exports.registerValidators = [
         }).trim(),
     body('name', 'Name should be longer 3 symbols.')
         .isLength({min: 3}).trim()
+]
+
+exports.loginValidators = [
+    body('email').isEmail().withMessage('Email is wrong.')
+        .custom(async (value, {req}) => {
+            try {
+                const user = await User.findOne({email: value})
+                if (!user) {
+                    return Promise.reject('Email is wrong.')
+                }
+            } catch (e) {
+                console.log(e)
+            }
+        })
+        .normalizeEmail(),
+    body('password', 'Password is wrong.')
+        .isLength({min: 6, max: 24}).isAlphanumeric().trim()
+        .custom(async (value, {req}) => {
+            try {
+                const user = await User.findOne({email: req.body.email})
+                const areSame = await bcrypt.compare(value, user.password)
+                if (!areSame) {
+                    return Promise.reject('Password is wrong.')
+                }
+                return true
+            } catch (e) {
+                console.log(e)
+            }
+        }),
 ]
