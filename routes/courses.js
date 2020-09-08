74d24ad68364a6973eb71b3c4d3a381fe875519c
Obazure <conprefor@gmail.com>
2020-09-08 21:48:11 +0200

    access & permission for editing courses.hbs

--- routes/courses.js
+++ routes/courses.js
@@ -3,17 +3,25 @@ const auth = require('../middleware/auth')
 const Course = require('../models/course')
 const router = Router()
 
+function isOwner(course, req) {
+    return course.userId.toString() === req.user._id.toString()
+}
+
 // index
 router.get('/', async (req, res) => {
-    const courses = await Course.find()
-        .populate('userId', 'name email')
-        // .select('')
-        .lean()
-    res.render('courses', {
-        title: ' Courses',
-        isCourses: true,
-        courses
-    })
+    try {
+        const courses = await Course.find()
+            .populate('userId', 'name email')
+            .lean()
+        res.render('courses', {
+            title: ' Courses',
+            isCourses: true,
+            userId: req.user ? req.user._id.toString() : null,
+            courses
+        })
+    } catch (e) {
+        console.log(e)
+    }
 })
 
 // create
@@ -55,11 +63,19 @@ router.get('/:id/edit', auth, async (req, res) => {
     if (!req.query.allow) {
         return res.redirect('/courses')
     }
-    const course = await Course.findById(req.params.id).lean()
-    res.render('course-edit', {
-        title: `Edit course: ${course.title}`,
-        course
-    })
+
+    try {
+        const course = await Course.findById(req.params.id).lean()
+
+        if (!isOwner(course, req)) return res.redirect('/courses')
+
+        res.render('course-edit', {
+            title: `Edit course: ${course.title}`,
+            course
+        })
+    } catch (e) {
+        console.log(e)
+    }
 })
 
 // update
@@ -67,8 +83,18 @@ router.post('/:id', auth, async (req, res) => {
     if (!req.query.allow || !req.params.id) {
         return res.redirect('/courses')
     }
-    await Course.findByIdAndUpdate(req.params.id, req.body)
-    res.redirect('/courses')
+    try {
+        const course = await Course.findById(req.params.id)
+
+        if (!isOwner(course, req)) return res.redirect('/courses')
+
+        Object.assign(course, req.body)
+        await course.save()
+        res.redirect('/courses')
+
+    } catch (e) {
+        console.log(e)
+    }
 })
 
 //destroy
