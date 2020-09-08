1c4c23ed00c2bd27d613689fc528660be10f9338
Obazure <conprefor@gmail.com>
2020-09-08 23:33:00 +0200

    express-validator data sanitazing

--- utils/validators.js
+++ utils/validators.js
@@ -2,22 +2,27 @@ const {body, validationResult} = require('express-validator/check')
 const User = require('../models/user')
 
 exports.registerValidators = [
-    body('email').isEmail().withMessage('Wrong Email.').custom(async (value, {req}) => {
-        try {
-            const user = await User.findOne({email: value})
-            if (user) {
-                return Promise.reject('Email is busy.')
+    body('email').isEmail().withMessage('Wrong Email.')
+        .custom(async (value, {req}) => {
+            try {
+                const user = await User.findOne({email: value})
+                if (user) {
+                    return Promise.reject('Email is busy.')
+                }
+            } catch (e) {
+                console.log(e)
             }
-        } catch (e) {
-            console.log(e)
-        }
-    }),
-    body('password', 'Min length for password is 6 symbols.').isLength({min: 6, max: 24}).isAlphanumeric(),
-    body('confirm').custom((value, {req}) => {
-        if (value !== req.body.password) {
-            throw new Error('Given passwords are not the same.')
-        }
-        return true
-    }),
-    body('name', 'Name should be longer 3 symbols.').isLength({min: 3})
+        })
+        .normalizeEmail(),
+    body('password', 'Min length for password is 6 symbols.')
+        .isLength({min: 6, max: 24}).isAlphanumeric().trim(),
+    body('confirm')
+        .custom((value, {req}) => {
+            if (value !== req.body.password) {
+                throw new Error('Given passwords are not the same.')
+            }
+            return true
+        }).trim(),
+    body('name', 'Name should be longer 3 symbols.')
+        .isLength({min: 3}).trim()
 ]
